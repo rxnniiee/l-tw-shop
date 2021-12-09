5649a97871048790f102bbaa979e839b6574d68d
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2021-12-10 00:39:17 +0200

    made height fit to content & passing style down from component

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -4,8 +4,8 @@ import styles from './Container.module.css'
 const Container = props => {
   return (
     <div
-      className={styles.main}
-      style={{ maxWidth: props?.maxWidth || '100%' }}
+      className={`${styles.main} ${props?.className ? props.className : ''}`}
+      style={props.style && props.style}
     >
       {props.title && <h4 className={styles.title}>{props.title}</h4>}
       {props.children}
