7a00450cfc1c163072b452c32929ee9d4a8eca8a
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2021-12-10 00:45:24 +0200

    display Products from ProductGrid directly, wtf was I thinking

--- src/components/product/ProductGrid.jsx
+++ src/components/product/ProductGrid.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import styles from './ProductGrid.module.css'
+import { useSelector } from 'react-redux'
+import Product from './Product'
 
 const ProductGrid = props => {
-  return <div className={styles.grid}>{props.children}</div>
+  const products = useSelector(state => state.products.list)
+  return (
+    <div className={styles.grid}>
+      {products.map(product => (
+        <Product key={product.id} {...product} />
+      ))}
+    </div>
+  )
 }
 
 export default ProductGrid
